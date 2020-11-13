677d16ea9b0ddbd254e8fbbfc3504562952ed114
MOHQ <73744329+MOHQ-TM@users.noreply.github.com>
2020-11-13 17:42:24 -0500

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,12 @@
+clear
+echo "[!] Install Python..."
+pkg install python -y
+clear
+echo "[!] Install Models..."
+pip install pyautogui
+clear
+pip install time 
+clear
+pip install os
+clear
+echo "Ok!!!"
